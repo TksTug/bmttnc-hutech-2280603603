0bb68cc9021da15fc28742ad767b906f3a07a379
TksTug <vthanhtung556@gmail.com>
2025-05-27 16:24:26 +0700

    bailab03

--- lab02/api.py
+++ lab02/api.py
@@ -4,8 +4,10 @@ from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
 from cipher.transposition import TranspositionCipher
+
 app = Flask(__name__)
-# CAESAR 
+
+# ========== CAESAR ==========
 caesar_cipher = CaesarCipher()
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
@@ -24,12 +26,9 @@ def caesar_decrypt():
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
-#main function
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-# VIGENERE 
+# ========== VIGENERE ==========
 vigenere_cipher = VigenereCipher()
+
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data = request.json
@@ -37,6 +36,7 @@ def vigenere_encrypt():
     key = data['key']
     encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
+
 @app.route('/api/vigenere/decrypt', methods=['POST'])
 def vigenere_decrypt():
     data = request.json
@@ -45,31 +45,35 @@ def vigenere_decrypt():
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
-#RAILFENCE 
+# ========== RAIL FENCE ==========
 railfence_cipher = RailFenceCipher()
+
 @app.route('/api/railfence/encrypt', methods=['POST'])
-def encrypt():
+def railfence_encrypt():
     data = request.json
     plain_text = data['plain_text']
     key = int(data['key'])
     encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
+
 @app.route('/api/railfence/decrypt', methods=['POST'])
-def decrypt():
+def railfence_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
-#PLAYFAIR 
 
+# ========== PLAYFAIR ==========
 playfair_cipher = PlayFairCipher()
+
 @app.route('/api/playfair/creatematrix', methods=['POST'])
 def playfair_creatematrix():
     data = request.json
     key = data['key']
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     return jsonify({"playfair_matrix": playfair_matrix})
+
 @app.route('/api/playfair/encrypt', methods=['POST'])
 def playfair_encrypt():
     data = request.json
@@ -78,6 +82,7 @@ def playfair_encrypt():
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
     return jsonify({'encrypted_text': encrypted_text})
+
 @app.route('/api/playfair/decrypt', methods=['POST'])
 def playfair_decrypt():
     data = request.json
@@ -86,9 +91,10 @@ def playfair_decrypt():
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
     return jsonify({'decrypted_text': decrypted_text})
-#TRANSPOSITION 
 
+# ========== TRANSPOSITION ==========
 transposition_cipher = TranspositionCipher()
+
 @app.route('/api/transposition/encrypt', methods=['POST'])
 def transposition_encrypt():
     data = request.get_json()
@@ -96,6 +102,7 @@ def transposition_encrypt():
     key = int(data.get('key'))
     encrypted_text = transposition_cipher.encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
+
 @app.route('/api/transposition/decrypt', methods=['POST'])
 def transposition_decrypt():
     data = request.get_json()
@@ -103,5 +110,7 @@ def transposition_decrypt():
     key = int(data.get('key'))
     decrypted_text = transposition_cipher.decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+# ========== MAIN ==========
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
