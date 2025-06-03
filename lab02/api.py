6682ef2810425b6d10ad68f0b5d60eb5f0e4fc2c
TksTug <vthanhtung556@gmail.com>
2025-06-03 16:27:17 +0700

    lab03 ne

--- lab02/api.py
+++ lab02/api.py
@@ -93,23 +93,23 @@ def playfair_decrypt():
     return jsonify({'decrypted_text': decrypted_text})
 
 # ========== TRANSPOSITION ==========
-transposition_cipher = TranspositionCipher()
+# transposition_cipher = TranspositionCipher()
 
-@app.route('/api/transposition/encrypt', methods=['POST'])
-def transposition_encrypt():
-    data = request.get_json()
-    plain_text = data.get('plain_text')
-    key = int(data.get('key'))
-    encrypted_text = transposition_cipher.encrypt(plain_text, key)
-    return jsonify({'encrypted_text': encrypted_text})
+# @app.route('/api/transposition/encrypt', methods=['POST'])
+# def transposition_encrypt():
+#     data = request.get_json()
+#     plain_text = data.get('plain_text')
+#     key = int(data.get('key'))
+#     encrypted_text = transposition_cipher.encrypt(plain_text, key)
+#     return jsonify({'encrypted_text': encrypted_text})
 
-@app.route('/api/transposition/decrypt', methods=['POST'])
-def transposition_decrypt():
-    data = request.get_json()
-    cipher_text = data.get('cipher_text')
-    key = int(data.get('key'))
-    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
-    return jsonify({'decrypted_text': decrypted_text})
+# @app.route('/api/transposition/decrypt', methods=['POST'])
+# def transposition_decrypt():
+#     data = request.get_json()
+#     cipher_text = data.get('cipher_text')
+#     key = int(data.get('key'))
+#     decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+#     return jsonify({'decrypted_text': decrypted_text})
 
 # ========== MAIN ==========
 if __name__ == "__main__":
